13f5d78cd0cfb9e0604a1ba9a762510a79dab01a
wdhowe <wdhowe@protonmail.com>
2020-12-23 23:31:03 -0600

    Initial add of example clj-tools project.

---
+++ test/clj_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-project.core-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [clj-project.core :as core]))
+
+(deftest -main-test
+  (testing "Fixme, I fail."
+    (is (= false true))))
