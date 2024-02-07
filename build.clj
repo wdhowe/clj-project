5e3cbe1918851977aa1134813b458624f1a03a7f
Bill Howe <4631165+wdhowe@users.noreply.github.com>
2024-02-06 21:56:04 -0600

    build-clj (#3)
    * build-clj
    
    - Overhaul build-clj to support uberjars.
    - Additional readme instructions for uberjars and clean.
    - Correct github workflow build command.

--- build.clj
+++ build.clj
@@ -1,84 +1,131 @@
 (ns build
-  "build script.
-  
-   Test
+  "Clean:
+   clj -T:build clean
+
+   Test:
    clj -T:build test
 
-   Test, Write POM, Build
-   clj -T:build ci
+   Test, Write POM, Build JAR:
+   clj -T:build jar
 
-   Deploy to Clojars
-   CLOJARS_USERNAME=username CLOJARS_PASSWORD=clojars-token clj -T:build deploy"
+   Test, Write POM, Build UberJAR:
+   clj -T:build uberjar
+
+   Deploy to Clojars:
+   CLOJARS_USERNAME=username
+   CLOJARS_PASSWORD=clojars-token
+   clj -T:build deploy"
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]
             [deps-deploy.deps-deploy :as dd]))
 
 (def project
   "Project Metadata"
-  {:lib (symbol "com.github.wdhowe/clj-project") 
+  {:description "A template for starting a clj deps/tools based project."
+   :lib 'com.github.wdhowe/clj-project
+   :main 'clj-project.core
    :version "1.0.0"
+   :license "Eclipse Public License"
+   :license-url "http://www.eclipse.org/legal/epl-v20.html"
    :author "Bill Howe"
-   :username "wdhowe" 
-   :git-repo "wdhowe/clj-project"})
+   :url "https://github.com/wdhowe/clj-project"
+   :git-repo "wdhowe/clj-project"
+   :class-dir "target/classes"})
 
-(defn- the-version [patch] (format "1.0.%s" patch))
-(def snapshot (the-version "999-SNAPSHOT"))
-(def class-dir "target/classes")
+(defn- pom-template
+  [version]
+  (let [git-repo (:git-repo project)
+        url (:url project)]
+   [[:description (:description project)]
+    [:url url]
+    [:licenses
+     [:license
+      [:name (:license project)]
+      [:url (:license-url project)]]]
+    [:developers
+     [:developer
+      [:name (:author project)]]]
+    [:scm
+     [:url url]
+     [:connection (format "scm:git:%s.git" url)]
+     [:developerConnection (format "scm:git:ssh:git@github.com:%s.git" git-repo)]
+     [:tag (str "v" version)]]]))
+
+(defn- jar-opts
+  [opts]
+  (let [version (:version project)
+        lib     (:lib project)]
+    (assoc opts
+           :basis     (b/create-basis {})
+           :class-dir (:class-dir project)
+           :jar-file  (format "target/%s-%s.jar" lib version)
+           :lib       lib
+           :pom-data  (pom-template version)
+           :src-dirs  ["src"]
+           :target    "target"
+           :version   version)))
+
+(defn- uber-opts [opts]
+  (let [version (:version project)
+        lib     (:lib project)]
+  (assoc opts
+         :basis      (b/create-basis {})
+         :class-dir  (:class-dir project)
+         :lib        lib
+         :main       (:main project)
+         :ns-compile [(:main project)]
+         :pom-data   (pom-template version)
+         :src-dirs   ["src"]
+         :uber-file  (format "target/%s-%s-standalone.jar" lib version)
+         :version    version)))
+
+(defn clean
+  "Remove build directory."
+  [_]
+  (b/delete {:path "target"}))
 
 (defn test
   "Run all the tests."
   [opts]
   (let [basis (b/create-basis {:aliases [:test]})
         cmds  (b/java-command
-                {:basis     basis
-                 :main      'clojure.main
-                 :main-args ["-m" "cognitect.test-runner"]})
+               {:basis     basis
+                :main      'clojure.main
+                :main-args ["-m" "cognitect.test-runner"]})
         {:keys [exit]} (b/process cmds)]
     (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
   opts)
 
-(defn- pom-template
-  [version]
-  [[:description "A library of functions to enhance clojure.core."]
-   [:url (format "https://github.com/%s" (:git-repo project))]
-   [:licenses
-    [:license
-     [:name "Eclipse Public License"]
-     [:url "http://www.eclipse.org/legal/epl-v20.html"]]]
-   [:developers
-    [:developer
-     [:name (:author project)]]]
-   [:scm
-    [:url (format "https://github.com/%s" (:git-repo project))]
-    [:connection (format "scm:git:https://github.com/%s.git" (:git-repo project))]
-    [:developerConnection (format "scm:git:ssh:git@github.com:%s.git" (:git-repo project))]
-    [:tag (str "v" version)]]])
-
-(defn- jar-opts
-  [opts]
-  (let [version (if (:snapshot opts) snapshot (:version project))]
-    (assoc opts
-           :lib (:lib project)
-           :version version
-           :jar-file  (format "target/%s-%s.jar" (:lib project) version)
-           :basis     (b/create-basis {})
-           :class-dir class-dir
-           :target    "target"
-           :src-dirs  ["src"]
-           :pom-data  (pom-template version))))
-
-(defn ci
+(defn jar
   "Run the CI pipeline of: tests, write pom, and build the JAR."
   [opts]
   (test opts)
-  (b/delete {:path "target"})
+  (clean nil)
   (let [opts (jar-opts opts)]
     (println "\nWriting pom.xml...")
     (b/write-pom opts)
     (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"] :target-dir class-dir})
-    (println "\nBuilding" (:jar-file opts) "...")
+    (b/copy-dir {:src-dirs ["resources" "src"]
+                 :target-dir (:class-dir project)})
+    (println "\nBuilding JAR" (:jar-file opts) "...")
     (b/jar opts))
+  opts)
+
+(defn uberjar
+  "Run the CI pipeline of tests (and build the uberjar)."
+  [opts]
+  (test opts)
+  (clean nil)
+  (let [opts (uber-opts opts)]
+    (println "\nWriting pom.xml...")
+    (b/write-pom opts)
+    (println "\nCopying source...")
+    (b/copy-dir {:src-dirs ["resources" "src"]
+                 :target-dir (:class-dir project)})
+    (println (str "\nCompiling " (:main project) "..."))
+    (b/compile-clj opts)
+    (println "\nBuilding UberJAR...")
+    (b/uber opts))
   opts)
 
 (defn deploy
