3bc8b5c5712c1036e5ceabde2bc66d13d26f698e
Bill Howe <bdhowe246@gmail.com>
2026-02-28 16:13:54 -0600

    Refactor build.clj: git-versioning, config cleanup, and uberjar extraction
    - Derive version from git tags with resources/VERSION fallback
    - Sort project map keys logically and use them consistently
      (eliminates hardcoded paths in jar-opts, uber-opts, copy-dir)
    - Extract shared build-uberjar helper; split uberjar into uber and ci
    - Expand .gitignore with additional IDE/tool patterns
    - Update all docstrings to Google style

--- build.clj
+++ build.clj
@@ -8,130 +8,217 @@
    Test, Write POM, Build JAR:
    clj -T:build jar
 
+   Build UberJAR:
+   clj -T:build uber
+
    Test, Write POM, Build UberJAR:
-   clj -T:build uberjar
+   clj -T:build ci
 
    Deploy to Clojars:
    CLOJARS_USERNAME=username
    CLOJARS_PASSWORD=clojars-token
    clj -T:build deploy"
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.build.api :as b]
+  (:require [clojure.java.shell :as shell]
+            [clojure.string :as string]
+            [clojure.tools.build.api :as b]
             [deps-deploy.deps-deploy :as dd]))
 
+;;; Project Configuration ;;;
+
+(defn- git-version
+  "Derive version from git tags.
+
+   Returns:
+     Version string, or nil if no matching tag found."
+  []
+  (try
+    (let [{:keys [exit out]} (shell/sh "git" "describe" "--tags" "--match" "v*" "--always")]
+      (when (zero? exit)
+        (-> out string/trim (string/replace-first #"^v" ""))))
+    (catch Exception _ nil)))
+
+(def version
+  "Project version.
+
+   Derived from git tags with fallback to resources/VERSION."
+  (or (git-version)
+      (-> (slurp "resources/VERSION") string/trim)))
+
 (def project
-  "Project Metadata"
-  {:description "A template for starting a clj deps/tools based project."
-   :lib 'com.github.wdhowe/clj-project
+  "Project metadata."
+  {:lib 'com.github.wdhowe/clj-project
+   :version version
+   :description "A template for starting a clj deps/tools based project."
    :main 'clj-project.core
-   :version "1.0.0"
-   :license "Eclipse Public License"
-   :license-url "http://www.eclipse.org/legal/epl-v20.html"
    :author "Bill Howe"
    :url "https://github.com/wdhowe/clj-project"
    :git-repo "wdhowe/clj-project"
+   :license "Eclipse Public License"
+   :license-url "http://www.eclipse.org/legal/epl-v20.html"
+   :src-dirs ["src"]
+   :resource-dirs ["resources"]
+   :target-dir "target"
    :class-dir "target/classes"})
 
 (defn- pom-template
-  [version]
-  (let [git-repo (:git-repo project)
-        url (:url project)]
-   [[:description (:description project)]
-    [:url url]
-    [:licenses
-     [:license
-      [:name (:license project)]
-      [:url (:license-url project)]]]
-    [:developers
-     [:developer
-      [:name (:author project)]]]
-    [:scm
-     [:url url]
-     [:connection (format "scm:git:%s.git" url)]
-     [:developerConnection (format "scm:git:ssh:git@github.com:%s.git" git-repo)]
-     [:tag (str "v" version)]]]))
+  []
+  [[:description (:description project)]
+   [:url (:url project)]
+   [:licenses
+    [:license
+     [:name (:license project)]
+     [:url (:license-url project)]]]
+   [:developers
+    [:developer
+     [:name (:author project)]]]
+   [:scm
+    [:url (:url project)]
+    [:connection (format "scm:git:%s.git" (:url project))]
+    [:developerConnection (format "scm:git:ssh:git@github.com:%s.git" (:git-repo project))]
+    [:tag (str "v" (:version project))]]])
 
 (defn- jar-opts
   [opts]
-  (let [version (:version project)
-        lib     (:lib project)]
-    (assoc opts
-           :basis     (b/create-basis {})
-           :class-dir (:class-dir project)
-           :jar-file  (format "target/%s-%s.jar" lib version)
-           :lib       lib
-           :pom-data  (pom-template version)
-           :src-dirs  ["src"]
-           :target    "target"
-           :version   version)))
+  (assoc opts
+         :lib       (:lib project)
+         :version   (:version project)
+         :basis     (b/create-basis {})
+         :jar-file  (format "%s/%s-%s.jar" (:target-dir project) (:lib project) (:version project))
+         :pom-data  (pom-template)
+         :src-dirs  (:src-dirs project)
+         :class-dir (:class-dir project)
+         :target    (:target-dir project)))
 
 (defn- uber-opts [opts]
-  (let [version (:version project)
-        lib     (:lib project)]
   (assoc opts
-         :basis      (b/create-basis {})
-         :class-dir  (:class-dir project)
-         :lib        lib
-         :main       (:main project)
+         :lib (:lib project)
+         :version (:version project)
+         :main (:main project)
+         :basis (b/create-basis {})
          :ns-compile [(:main project)]
-         :pom-data   (pom-template version)
-         :src-dirs   ["src"]
-         :uber-file  (format "target/%s-%s-standalone.jar" lib version)
-         :version    version)))
+         :pom-data (pom-template)
+         :uber-file (format "%s/%s-%s.jar" (:target-dir project) (:lib project) (:version project))
+         :src-dirs (:src-dirs project)
+         :class-dir (:class-dir project)))
+
+;;; Build Tasks ;;;
 
 (defn clean
-  "Remove build directory."
-  [_]
-  (b/delete {:path "target"}))
+  "Clean build artifacts.
+
+   Args:
+     opts: Build options map.
+
+   Returns:
+     opts."
+  [opts]
+  (println "Cleaning build artifacts...")
+  (b/delete {:path (:target-dir project)})
+  opts)
 
 (defn test
-  "Run all the tests."
+  "Run all the tests.
+
+   Args:
+     opts: Build options map.
+
+   Returns:
+     opts.
+
+   Throws:
+     ExceptionInfo if tests fail."
   [opts]
   (let [basis (b/create-basis {:aliases [:test]})
-        cmds  (b/java-command
-               {:basis     basis
-                :main      'clojure.main
-                :main-args ["-m" "cognitect.test-runner"]})
+        cmds (b/java-command
+              {:basis     basis
+               :main      'clojure.main
+               :main-args ["-m" "cognitect.test-runner"]})
         {:keys [exit]} (b/process cmds)]
     (when-not (zero? exit) (throw (ex-info "Tests failed" {}))))
   opts)
 
 (defn jar
-  "Run the CI pipeline of: tests, write pom, and build the JAR."
+  "Run the CI pipeline of tests, write pom, and build the JAR.
+
+   Args:
+     opts: Build options map.
+
+   Returns:
+     opts."
   [opts]
   (test opts)
-  (clean nil)
+  (clean opts)
   (let [opts (jar-opts opts)]
     (println "\nWriting pom.xml...")
     (b/write-pom opts)
     (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"]
+    (b/copy-dir {:src-dirs (into (:resource-dirs project) (:src-dirs project))
                  :target-dir (:class-dir project)})
     (println "\nBuilding JAR" (:jar-file opts) "...")
     (b/jar opts)
     (println (format "Created %s" (:jar-file opts))))
   opts)
 
-(defn uberjar
-  "Run the CI pipeline of tests (and build the uberjar)."
+(defn- build-uberjar
+  "Build uberjar from resolved opts.
+
+   Copies source, writes VERSION, compiles, and packages.
+
+   Args:
+     opts: Resolved uber-opts map.
+
+   Returns:
+     opts."
   [opts]
-  (test opts)
-  (clean nil)
-  (let [opts (uber-opts opts)]
-    (println "\nWriting pom.xml...")
-    (b/write-pom opts)
-    (println "\nCopying source...")
-    (b/copy-dir {:src-dirs ["resources" "src"]
-                 :target-dir (:class-dir project)})
-    (println (str "\nCompiling " (:main project) "..."))
-    (b/compile-clj opts)
-    (println "\nBuilding UberJAR...")
-    (b/uber opts)
-    (println (format "Created %s" (:uber-file opts))))
+  (println (str "Version: " version))
+  (println "Copying source...")
+  (b/copy-dir {:src-dirs (into (:resource-dirs project) (:src-dirs project))
+               :target-dir (:class-dir project)})
+  (spit (str (:class-dir project) "/VERSION") version)
+  (println (str "Compiling " (:main project) "..."))
+  (b/compile-clj opts)
+  (println (str "Building UberJAR... " (:uber-file opts)))
+  (b/uber opts)
+  (println (format "Created %s" (:uber-file opts)))
   opts)
 
+(defn uber
+  "Build the uberjar.
+
+   Args:
+     opts: Build options map.
+
+   Returns:
+     opts."
+  [opts]
+  (clean opts)
+  (build-uberjar (uber-opts opts)))
+
+(defn ci
+  "Run the CI pipeline of tests, write pom, and build the uberjar.
+
+   Args:
+     opts: Build options map.
+
+   Returns:
+     opts."
+  [opts]
+  (test opts)
+  (clean opts)
+  (let [opts (uber-opts opts)]
+    (println "Writing pom.xml...")
+    (b/write-pom opts)
+    (build-uberjar opts)))
+
 (defn deploy
-  "Deploy the JAR to Clojars."
+  "Deploy the JAR to Clojars.
+
+   Args:
+     opts: Build options map.
+
+   Returns:
+     opts."
   [opts]
   (let [{:keys [jar-file] :as opts}
         (jar-opts opts)]
